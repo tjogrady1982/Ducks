4beefffe5e002a282c5b631d5f08020bb3957c89
tjogrady1982 <38034744+tjogrady1982@users.noreply.github.com>
2018-06-12 14:22:06 +0100

    Add files via upload

---
+++ Ducks/Ducks/Duck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+    public class Duck : IEquatable<Duck>
+    {
+        public string Name { get; }
+        public string Type { get; }
+        public int WeightInGrams { get; }
+        public int AgeInMonths { get; }
+
+        public Duck(string name, string type, int weightInGrams, int ageInMonths)
+        {
+            Name = name;
+            Type = type;
+            WeightInGrams = weightInGrams;
+            AgeInMonths = ageInMonths;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Duck)obj);
+        }
+
+        public bool Equals(Duck other)
+        {
+            return WeightInGrams == other.WeightInGrams;
+        }
+
+        public override int GetHashCode()
+        {
+            return WeightInGrams;
+        }
+    }
+}
---
+++ Ducks/Ducks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quack = new Duck("Sir Quackalot", "Mallard", 100, 17);
+            var quacker = new Duck("Quackers","Eider", 100, 12);
+            var quackers = new Duck("Orange", "Mallard", 101, 17);
+
+            if (quack.Equals(quackers))//weight comparer
+            {
+                Console.WriteLine("a match!");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("no match!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
